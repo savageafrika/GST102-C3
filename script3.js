e12bf5f53fe9a063c77f6c1f3a8e59b460504fd5
savageafrika <113559289+savageafrika@users.noreply.github.com>
2023-11-17 03:40:26 +0100

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,299 @@
+
+
+
+
+ 
+
+   //Questions array
+
+   let questions = [
+
+    {
+
+         number:1,
+
+         question: "Nigerian English has been brought about by the __",
+
+         answer : "Multiplicity of languages",
+
+         options : [
+
+                          "Defects in education system",
+
+                          "Government corruption",
+
+                          "Multiplicity of languages",
+
+                          "Neocolonialism"
+
+                        ]
+
+    
+
+    },
+
+    {
+
+         number:2,
+
+         question: "Who referred to Nigerian English as a misnomer",
+
+         answer : "Fodeh Baldeh",
+
+         options : [
+
+                          "William Wordsworth",
+
+                          "John Keats",
+
+                          "Fodeh Baldeh",
+
+                          "Lord Tennyson"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:3,
+
+         question: "These factors necessary errors in the use of English except",
+
+         answer : "Multiplicity of languages",
+
+         options : [
+
+                          "Multiplicity of languages",
+
+                          "Problem of orthography",
+
+                          "Mother tongue interference",
+
+                          "Problem of borrowed words"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:4,
+
+         question: "I am not hearing you” the error here is caused by",
+
+         answer : "Mother tongue interference",
+
+         options : [
+
+                          "Multiplicity of languages",
+
+                          "Problem of orthography",
+
+                          "Mother tongue interference",
+
+                          "Problem of borrowed words"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:5,
+
+         question: "They entered into a convenant . The error here is necessitated by",
+
+         answer : "Problem of orthography",
+
+         options : [
+
+                          "Multiplicity of languages",
+
+                          "Problem of orthography",
+
+                          "Mother tongue interference",
+
+                          "Problem of borrowed words"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:6,
+
+         question: "Sound system or structure is also known as",
+
+         answer : "Orthography",
+
+         options : [
+
+                          "Syntax",
+
+                          "Morphology",
+
+                          "Orthography",
+
+                          "Phonology"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:7,
+
+         question: "_&_ critically considered in the use of Concord",
+
+         answer : "Gender and number",
+
+         options : [
+
+                          "Syntax and semantics",
+
+                          "Subject and verb",
+
+                          "Phrases and clauses",
+
+                          "Gender and number"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:8,
+
+         question: "Which is the correct expression",
+
+         answer : "If one studies hard, one will pass",
+
+         options : [
+
+                          "If one studies hard, he will pass",
+
+                          "If one studies hard, one will pass",
+
+                          "If one studies hard, they will pass",
+
+                          "If one studies is hard, each will pass"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:9,
+
+         question: "Which is the correct statement",
+
+         answer : "Either James and John or I am to blame",
+
+         options : [
+
+                          "Either James and John or I is to blame",
+
+                          "Either James and John or I are to blame",
+
+                          "Either James and John or I were to blame",
+
+                          "Either James and John or I am to blame"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:10,
+
+         question: "Choose the correct expression,",
+
+         answer : "Tell me what are you discussing",
+
+         options : [
+
+                          "Tell me what are you discussing about",
+
+                          "Tell me what you are discussing about",
+
+                          "Tell me what are you discussing",
+
+                          "Tell me what you are discussing"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:11,
+
+         question: "Which is the correct expression",
+
+         answer : "Jane prefers rice to beans",
+
+         options : [
+
+                          "Jane prefers rice than beans",
+
+                          "Jane prefers rice to beans",
+
+                          "Jane prefers rice over beans ",
+
+                          "Jane prefers rice more than beans"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:12,
+
+         question: "Which of these statements is wrongly punctuated",
+
+         answer : "Oh my god, what a day.",
+
+         options : [
+
+                          "Oh my god, what a day.",
+
+                          "Please, may I sit with you.",
+
+                          "What is your name?",
+
+                          "Slow, machine at work."  
+
+                          ]
+
+    
+
+    }
+
+     ];   
